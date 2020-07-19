0d0e2828a5028cf18adee34682448200cc0bd3ee
Eric Laursen <ericjlaursen@gmail.com>
2020-07-19 19:58:35 -0400

    more working

--- init-worker/ui/js/worker.js
+++ init-worker/ui/js/worker.js
@@ -1,11 +1,36 @@
 "use strict";
 
-var curNum = 0;
-
-self.postMessage('Hello from web-worker here!');
+let curNum = 0;
 
 self.onmessage = onMessage;
 
+//Once client sends a message, start the fib...
 function onMessage(e){
-	console.log(`Received in web-worker: ${JSON.stringify(e.data)}`);
+
+	//START the fib-finding sequence
+	getNextFib();
+}
+
+function getNextFib(){
+	//... this will get exponentially slower as the number grows
+	const curFib = makeFib(curNum);
+
+	//send msg to webpage
+	self.postMessage({idx: curNum, num: curFib})
+
+	// increase fibonacci index to create
+	curNum++;
+
+	//re-start fib-creating process
+	getNextFib()
+}
+
+//... an intentionally exponentially slow fibonacci creating fn
+function makeFib(n){
+	
+	if(n < 2){
+		return n;
+	}
+	
+	return makeFib(n - 1) + makeFib(n - 2);
 }
