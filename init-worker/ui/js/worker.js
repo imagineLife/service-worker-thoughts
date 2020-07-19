76c018edbff72a76e1e97a82f0cd34dc05cf1417
Eric Laursen <ericjlaursen@gmail.com>
2020-07-19 19:34:05 -0400

    posts AND shows message from ui

--- init-worker/ui/js/worker.js
+++ init-worker/ui/js/worker.js
@@ -3,3 +3,9 @@
 var curNum = 0;
 
 self.postMessage('Hello from web-worker here!');
+
+self.onmessage = onMessage;
+
+function onMessage(e){
+	console.log(`Received in web-worker: ${JSON.stringify(e.data)}`);
+}
