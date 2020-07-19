1f8d05d6b50ad22d35aff39d2e498e57d7dd41b7
Eric Laursen <ericjlaursen@gmail.com>
2020-07-19 19:28:39 -0400

    rename init worker

---
+++ init-worker/ui/js/index.js
@@ -0,0 +1,68 @@
+(function index(){
+	"use strict";
+
+	var startStopBtn;
+	var fibsList;
+
+	//holds the worker
+	var worker;
+
+	//set READY fn on loaded
+	document.addEventListener("DOMContentLoaded",ready,false);
+
+
+	// LISTEN on button for click,
+	// call 'startFibs'
+	function ready() {
+		startStopBtn = document.getElementById("start-stop-btn");
+		fibsList = document.getElementById("fibs");
+
+		startStopBtn.addEventListener("click",startFibs,false);
+	}
+
+	function renderFib(num,fib) {
+		var p = document.createElement("div");
+		p.innerText = `Fib(${num}): ${fib}`;
+		if (fibsList.childNodes.length > 0) {
+			fibsList.insertBefore(p,fibsList.childNodes[0]);
+		}
+		else {
+			fibsList.appendChild(p);
+		}
+	}
+
+	function startFibs() {
+		startStopBtn.removeEventListener("click",startFibs,false);
+		startStopBtn.addEventListener("click",stopFibs,false);
+
+		startStopBtn.innerText = "Stop";
+		fibsList.innerHTML = "";
+
+		//set worker sourced from js file
+		worker = new Worker("/js/worker.js");
+
+		//LISTEN for 'message' event
+		worker.addEventListener("message",onMessage);
+
+		//SEND a msg
+		worker.postMessage({ start: true });
+	}
+
+	function stopFibs() {
+		startStopBtn.removeEventListener("click",stopFibs,false);
+		startStopBtn.addEventListener("click",startFibs,false);
+
+		startStopBtn.innerText = "Start";
+		worker.terminate();
+		worker = null;
+	}
+
+	function onMessage(e) {
+		console.log('e')
+		console.log(e)
+		console.log(e.data);
+		// const {data: {num, fib}} = evt;
+		// renderFib(num,fib);
+	}
+
+})();
---
+++ init-worker/ui/js/worker.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var curNum = 0;
+
+self.postMessage('Hello from web-worker here!');
