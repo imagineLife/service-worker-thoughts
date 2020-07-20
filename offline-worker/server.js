83eed7cf646dc5637af02b2ea62edabf8803bf85
Eric Laursen <ericjlaursen@gmail.com>
2020-07-20 06:18:24 -0400

    vars

--- offline-worker/server.js
+++ offline-worker/server.js
@@ -8,6 +8,11 @@ const getStream = require("get-stream");
 const cookie = require("cookie");
 const rand = require("random-number-csprng");
 
+//primisified fns
 const fsReadDir = util.promisify(fs.readdir);
 const fsReadFile = util.promisify(fs.readFile);
 const fsWriteFile = util.promisify(fs.writeFile);
+
+//vars
+const PORT = 8049;
+const WEB_DIR = path.join(__dirname,"web");
