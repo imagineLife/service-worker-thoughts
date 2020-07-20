93b1aebe57c105138c38a980b50d21f9d7227e6c
Eric Laursen <ericjlaursen@gmail.com>
2020-07-20 06:19:27 -0400

    placeholder pages array

--- offline-worker/server.js
+++ offline-worker/server.js
@@ -16,3 +16,31 @@ const fsWriteFile = util.promisify(fs.writeFile);
 //vars
 const PORT = 8049;
 const WEB_DIR = path.join(__dirname,"web");
+
+//array of web-pages
+const pages = [
+	{
+		// basic static page friendly URL rewrites
+		match: /^\/(?:index)?(?:[#?]|$)/,
+		serve: "index.html",
+		force: true,
+	},
+	{
+		// basic static page friendly URL rewrites
+		match: /^\/(?:about|contact|login|404|offline)(?:[#?]|$)/,
+		serve: "<% basename %>.html",
+		force: true,
+	},
+	{
+		// URL rewrites for individual posts
+		match: /^\/post\/[\w\d-]+(?:[#?]|$)/,
+		serve: "posts/<% basename %>.html",
+		force: true,
+	},
+	{
+		// match (with force) static files
+		match: /^\/(?:(?:(?:js|css|images)\/.+))$/,
+		serve: ".<% reqPath %>",
+		force: true,
+	},
+]
